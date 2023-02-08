2eee1c6e50d6066f26bf4a4f2d26ec4a08215654
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-08 18:11:49 +0600

    Calculate sum of numbers using recursion practice

---
+++ simple.js
@@ -0,0 +1,26 @@
+// let sum = 0;
+// for (let i = 0; i <= 5; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+// for(let i = 5; i >= 1; i--){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+// Calculate sum of numbers using recursion
+
+
+
+// function sum(i){
+//     if (i == 1){
+//         return 1;
+//     }
+//     console.log(i);
+//     return i + sum(i-1);
+// }
+// const result = sum(5);
+// console.log(result);

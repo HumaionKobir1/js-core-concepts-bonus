81c60b7cec4568a079d8908ab0aff3a5b4ab9c27
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-08 18:23:40 +0600

    find factorial use  recursion function

---
+++ factorial.js
@@ -0,0 +1,18 @@
+// let factorial = 1;
+// for (let i = 5; i >= 1; i--){
+//     factorial = factorial * i;
+// }
+// console.log(factorial);
+
+
+
+// If the same function is called again from within a function, it is called a rection
+
+function factorial (i){
+    if(i == 1){
+        return 1;
+    }
+    return i * factorial(i - 1);
+}
+const result = factorial(5);
+console.log(result);
